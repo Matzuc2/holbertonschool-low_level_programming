94013f4d7290a549200aae2125295b0fff13c7b0
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 22:29:57 +0200

    adding 6 script

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+*	main - Prints "Programming is like building a multilingual
+ *	puzzle, followed by a new line.
+	* Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c = '0';
+
+	for (int i = 0 ; i < 10; i++)
+	{
+		putchar(c);
+		c++;
+	}
+putchar('\n');
+return (0);
+}

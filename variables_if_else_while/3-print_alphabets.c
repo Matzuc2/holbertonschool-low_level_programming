db469a29d5aea81c6b2fccd6c3430e454e6dfee2
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 17:04:14 +0200

    adding script 3 alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	char z = 'z';
+	char A;
+	char Z;
+	
+	for(a ='a'; a < 'z'; a++)
+	{
+	putchar(a);
+	}
+	for(A = 'A'; A <= 'Z' ; A++)
+	{
+	putchar(A);
+	}
+putchar('\n');
+return (0);
+}
+	

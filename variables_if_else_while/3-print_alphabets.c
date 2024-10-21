cbb73a351d75ae6a9847e2bd03537a1aaec70692
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 17:07:56 +0200

    adding script 3 alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
+/* more headers goes there */
+/**
+*	main - Prints "Programming is like building a multilingual
+ *	puzzle, followed by a new line.
+	* Return: Always 0.
+*/
+/* betty style doc for function main goes there */
 int main(void)
 {
 	char a;
 	char z = 'z';
 	char A;
 	char Z;
-	
-	for(a ='a'; a < 'z'; a++)
-	{
-	putchar(a);
-	}
-	for(A = 'A'; A <= 'Z' ; A++)
-	{
-	putchar(A);
-	}
+
+	for (a = 'a'; a < 'z'; a++)
+		putchar(a);
+
+	for (A = 'A'; A <= 'Z' ; A++)
+		putchar(A);
 putchar('\n');
 return (0);
 }
-	
+

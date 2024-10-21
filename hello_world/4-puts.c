e164ea1be51975b1c5ebb97c260aaeb1576fb2de
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 13:14:03 +0200

    adding script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 /**
-	 * This is the preferred style for multi-line
-	 * comments in C source code.
-	 * Please use it consistently.
-	 *
-	 * Description:  A column of asterisks on the left side,
-	 * with beginning and ending almost-blank lines.
 	 * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
 	 * Return: Always 0.

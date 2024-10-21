5e42ca9e3d8377957db1baf33633cf76532030d4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 14:41:23 +0200

    adding script 0 project 2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
+/**
+*	main - Prints "Programming is like building a multilingual
+ *	puzzle, followed by a new line.
+	* Return: Always 0.
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,9 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
+	if (n > 0)
 	{
 	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
 	}
 	else
 	{

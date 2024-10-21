fae818000afb1aa0b19cfeb95834f1e5681c7027
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 14:06:05 +0200

    adding script 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int longlongintType;
 	float floatType;
 
-	printf("Size of char: %zu byte\n",  sizeof(charType));
-	printf("Size of an int: %zu byte\n", sizeof(intType));
-	printf("Size of a long int : %zu byte\n", sizeof(longintType));
-	printf("Size of a long long int : %zu byte\n", sizeof(longlongintType));
-	printf("Size of a float: %zu byte\n", sizeof(floatType));
+	printf("Size of char: %zu byte(s)\n",  sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int : %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int : %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }

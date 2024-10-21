4a16212fdba0a078904f965afb8b33eaf644189f
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 13:13:09 +0200

    adding script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 /**
- * Description: function main that returns zero.
- */
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+	 * Return: Always 0.
+	 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

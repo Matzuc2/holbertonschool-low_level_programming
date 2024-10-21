b20d636e5d73b6af385268c3a3aeb034855b9282
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 22:37:18 +0200

    adding script 7-print_tebalpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+*	main - Prints "Programming is like building a multilingual
+ *	puzzle, followed by a new line.
+	* Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char z = 'z';
+	char a = 'a';
+
+	for (z = 'z' ; z >= a ; z--)
+	{
+	putchar(z);
+	}
+putchar('\n');
+return (0);
+}

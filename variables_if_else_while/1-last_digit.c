97781bf64873b2c374d47009c7161dee37e165da
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 15:08:22 +0200

    adding 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,17 +13,20 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n =  n % 10;
+	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
+	n =  n % 10;
 	printf("last digit of n is %d and is greater than 5\n", n);
 	}
 	else if (n == 0)
 	{
+	n =  n % 10;
 	printf("last digit of n is %d and is 0\n", n);
 	}
 	else if (n < 6 && n != 0)
 	{
+	n =  n % 10;
 	printf("last digit of n is %d and is less than 6 and not 0\n", n);
 	}
 	return (0);

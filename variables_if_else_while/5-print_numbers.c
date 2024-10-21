a9f78027465ca78788018e377162de6e54793676
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-21 21:23:06 +0200

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+*	main - Prints "Programming is like building a multilingual
+ *	puzzle, followed by a new line.
+	* Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int count = 0;
+	int i = 0;
+
+	for (i <= 1; i < 10; i++)
+	{
+	count++;
+	printf("%d", count);
+	}
+printf("\n");
+return (0);
+}

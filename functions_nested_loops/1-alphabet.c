7b71ca07214005272396e80efe53a7624962cf98
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-22 11:49:09 +0200

    adding 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,14 +2,18 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main - check the code
- *
+ * print_alphabet - print the alphabet
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	char c = '\n';
-	print_alphabet();
-	_putchar(c);
-	return (0);
+	int i;
+	char a = 'a';
+
+	for (i = 0 ; i < 26; i++)
+	{
+	_putchar(a);
+	a++;
+	}
+
 }

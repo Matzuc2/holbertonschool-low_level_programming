d569516b4bb75f5835bf0cc69372b9ad4812f37e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-22 17:22:40 +0200

    adding script 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _isalpha - print the alphabet ten times
+ * Return: Always 0.
+ * @c: int c.
+ */
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }

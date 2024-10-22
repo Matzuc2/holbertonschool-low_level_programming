1e21dc8097d7eff4477a9331402490f5231b36c4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-22 16:41:58 +0200

    allooooo

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 #include <ctype.h>
-
+/**
+ * _islower - print the alphabet ten times
+ * Return: Always 0.
+ * @c: int c.
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')

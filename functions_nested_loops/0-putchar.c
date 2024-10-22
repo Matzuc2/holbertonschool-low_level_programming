be81bb79bfcb63d0cd99378a3ef3010d78279e2b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-22 09:24:33 +0200

    adding script 0 putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+*	main - Prints "Programming is like building a multilingual
+ *	puzzle, followed by a new line.
+	* Return: Always 0.
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	printf("_putchar \n");
+	return (0);
+}

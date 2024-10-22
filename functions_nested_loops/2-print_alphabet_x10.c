4b52ffed2ac56b41618f1f57cd18a847bc7995a3
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-22 14:01:14 +0200

    adding script

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,21 @@
  * print_alphabet - print the alphabet
  * Return: Always 0.
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
+	int j = 0;
 	int i;
 	char a = 'a';
 
-	for (i = 0 ; i < 42; i++)
-	{
-	_putchar(a);
-	a++;
-	}
+		for (j = 0 ; j < 10; j++)
+		{
+		for (i = 0; i < 26; i++)
+		{
+		_putchar(a);
+		a++;
+		}
+ 		a = 'a';
+		}
+
 _putchar('\n');
 }

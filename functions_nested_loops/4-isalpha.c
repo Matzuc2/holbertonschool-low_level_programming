6ce056e63a46ca20541f0a5db330180182d0f466
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-22 17:19:51 +0200

    adding script 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,14 +3,12 @@
 
 int _isalpha(int c)
 {
-        c = 'a';
-
-        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }

5fba3893040e2acca9ffc8f24eaa38ad2345d842
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-23 13:28:49 +0200

    adding script 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - print the sign + or - depending of the number
+ * Return: return m.
+ *@i : int i.
+ *@j : int j.
+ */
+int add(int i, int j)
+{
+	int m;
+
+	m = i + j;
+	return (m);
+}

dbbd499785d0fe547c362300968b6f94aa3f3767
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-23 16:12:05 +0200

    project 3 is for crackheads

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print the sign + or - depending of the number
+ *@n : int n.
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n ; n <= 98 ; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (n ; n >= 98 ; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	printf("\n");
+	}
+}

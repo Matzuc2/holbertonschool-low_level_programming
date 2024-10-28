0dba3069f9957f91cde4c63e799eb83b70e72422
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-28 13:46:26 +0100

    adding square script

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
-
-
+/**
+ * print_square - print __ in function of imput n
+ * @size:int size
+*/
 void print_square(int size)
 {
 	char j = '#';
 	int i;
 	int h;
 
-		if( size > 0)
+		if (size > 0)
 		{
 			for (i = 0; i < size; i++)
 			{

6dc5a9b912cdc21e2b895aac887ad1d521a5dc01
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-28 14:31:30 +0100

    adding script 9 fizzbuz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print all numbers but switch these for strings in some cases
+ * Return: return 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 1 ; n <= 100 ; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+return (0);
+}

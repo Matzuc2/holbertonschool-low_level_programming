c15214a2018ac20215ff67107b462a2610df3a63
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-29 15:56:19 +0100

    adding script 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+*puts_half - prints the second half of a string
+*@str : char str (pointer)
+*/
+void puts_half(char *str)
+{
+	int len = 0;
+	int len2 = 0;
+	int i;
+
+
+	while (str[len] != '\0')
+	{
+		len++;
+		len2++;
+	}
+	len--;
+
+	for (i = 1; i < len; i++)
+	{
+		len--;
+		if (len == i)
+		{
+			break;
+		}
+	}
+	while (i < len2)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

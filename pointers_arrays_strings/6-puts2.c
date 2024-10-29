232c0bb21b4cdf2c038b6fde2ab6df88a157dc36
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-29 14:54:26 +0100

    adding script 6 and main.h

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+*puts2 - prints every two numbers in a string
+*@str : char str (pointer)
+*/
+void puts2(char *str)
+{
+	int len = 0;
+	int i;
+	int temp;
+
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

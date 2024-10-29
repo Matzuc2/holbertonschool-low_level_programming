d7676cc73851e567ab28638684397557b2958b17
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-29 23:00:58 +0100

    adding script 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		_putchar(src[i]);
+		dest += src[i];
+	}
+return (dest);
+}
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}

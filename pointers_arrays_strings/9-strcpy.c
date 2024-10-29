3bd2fc5897976c200b0a4e2f1feaffe40e8f6382
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-29 23:27:04 +0100

    adding script 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <unistd.h>
+/**
+*_strcpy - copy any string in return
+*@dest : dest is the copy of the string in return
+*@src : src is the string itself that dest has to copy
+*Return: returns the copy of the string by the pointer
+*/
 
 char *_strcpy(char *dest, char *src)
 {
@@ -7,8 +13,8 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		_putchar(src[i]);
-		dest += src[i];
+		dest[i] = src[i];
 	}
 return (dest);
 }
+

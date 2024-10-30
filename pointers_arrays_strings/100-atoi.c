bdec737404d0c314da548aaa7835bb850a2d06d4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-30 13:57:00 +0100

    atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,32 @@
 #include "main.h"
 #include <limits.h>
 
+#include "main.h"
+
 int _atoi(char *s)
 {
-  int result = 0;
-  int puiss = 1;
-  int i = 0;
-while (s[i] == ' ')
-	i++;
-while (s[i] == '+' || s[i] == '-')
-{
-      	if (s[i] == '-')
-      	{
-      		puiss *= -1;
-		i++;
+    int result = 0;
+    int puiss = 1;
+    int i = 0;
+    int found_digit = 0;
+
+    while (s[i] != '\0')
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10 + (s[i] - '0');
+            found_digit = 1;
         }
-	else
-	{
-	i++;
-	}
-}
-while (s[i] >= '0' && s[i] <= '9')
-{
-        result = result * 10 + (s[i] - '0');
-	i++;
-	}
-	return (result * puiss);
-}
+        else if (found_digit)
+        {
+            break;
+        }
+        else if (s[i] == '-' && !found_digit)
+        {
+            puiss *= -1;
+        }
+        i++;
+    }
 
-
+    return (result * puiss);
+}

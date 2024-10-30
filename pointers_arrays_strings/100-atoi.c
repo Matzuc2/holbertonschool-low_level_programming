22fed0c20df67c67995190c5cf9c57ecc951c93c
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-30 16:40:43 +0100

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,32 @@
 #include "main.h"
-#define INT_MAX 2147483647
-#define INT_MIN (-INT_MAX - 1)
 /**
 *_atoi - f*ck you atoi
 *@s: char s
 *Return: return only first integers in strings
 */
-#include "main.h"
-
 int _atoi(char *s)
 {
-    int result = 0;
-    int puiss = 1;
-    int i = 0;
-    int found_digit = 0;
+	int result = 0;
+	int sign = 1;
+	int started = 0;
 
-    while (s[i] != '\0')
-    {
-	if (result > 214748364 || (result == 214748364 && *s > '7'))
+	while (*s)
 	{
-		return ((puiss == 1) ? 2147483647 : -2147483648);
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			if (result > 214748364 || (result == 214748364 && *s > '7'))
+			{
+				return ((sign == 1) ? 2147483647 : -2147483648);
+			}
+			result = result * 10 + (*s - '0');
+		}
+		else if (started)
+			break;
+		s++;
 	}
-	if (result < -214748364 || (result == -214748364 && *s > '7'))
-        {
-                return ((puiss == 1) ? 2147483647 : -2147483648);
-        }
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
-            found_digit = 1;
-        }
-        else if (found_digit)
-        {
-            break;
-        }
-        else if (s[i] == '-' && !found_digit)
-        {
-            puiss *= -1;
-        }
-        i++;
-    }
 
-    return (result * puiss);
+	return (result * sign);
 }

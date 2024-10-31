cd36683bc499974723c3a97b68c7fb42956dee78
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-10-31 13:36:50 +0100

    aloa

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,27 @@
 #include "main.h"
-
+/**
+*_strcat - add a string withotu
+*@dest : char dest
+*@src : char src
+*Return: dest
+*/
 
 char *_strcat(char *dest, char *src)
 
 {
-int i;
-int j;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-
-	if (dest[i] == '\0')
+	for (i = 0 ; dest[i] != '\0'; i++)
 	{
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i] += src[j];
-		}
+
 	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] += src[j];
+		i++;
+	}
+
 dest[i] = '\0';
 return (dest);
 }

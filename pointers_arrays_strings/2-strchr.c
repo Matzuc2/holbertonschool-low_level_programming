a84638b14dec4e5c9313c0d59c0313d541df5fed
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-04 15:36:22 +0100

    hey

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,14 @@
 #include "main.h"
-#define NULL ( (void *) 0)
-
+#include <stdio.h>
+/**
+*_strchr - function that returns a string when pointer find c
+*@s: string to search with
+*@c: character to find
+*Return: returns the characters found + the rest of the string
+*/
 char *_strchr(char *s, char c)
 {
-	int i;x
+	int i;
 	/*int j = 0;*/
 
 	for (i = 0; s[i] != c ; i++)

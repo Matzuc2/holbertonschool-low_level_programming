7d8ec288b49629a646565b7a1158969eed847088
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-06 00:20:00 +0100

    hey

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,11 +6,6 @@
 *@a: pointer containing the fcking array
 */
 void print_chessboard(char (*a)[8])
-/* RAPPEL, ici le pointeur pointera toujours sur a[0] si on l*/
-/*l'indexe, malheureusement ce format-là la 2D array ne permet pas d'indexer*/
-/*(ou du moins pas comme je le souhaite).*/
-/*on définit des variables simples qui nous serviront à la place*/
-/*comme rows ou columns. Puis on affiche quand on a les bonnes valeurs.*/
 {
 	int rows;
 	int columns;

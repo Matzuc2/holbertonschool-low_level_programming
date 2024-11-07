ced40b16b9364d01e7771ce177d14091a8e6ff71
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-07 18:01:58 +0100

    adding script 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include "main.h"
 /**
+*main - print the program name
+*Return: return 0
+*@argc: return the number of char in appelation of program
+*@argv: return the name of argv[i]
 */
 int main(int argc, char **argv)
 {

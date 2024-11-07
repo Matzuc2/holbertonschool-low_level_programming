65e687211a7d9b9c1c73f49fa953af0ed8472963
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-07 20:56:58 +0100

    allo allo

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - print the program name
+*Return: return 0
+*@argc: return the number of char in appelation of program
+*@argv: return the name of argv[i]
+*/
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}

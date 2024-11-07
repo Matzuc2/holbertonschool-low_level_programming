ac1b313353ef710a373afe5d00e8af9deda9e6d6
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-07 18:11:46 +0100

    adding script 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
 *@argc: return the number of char in appelation of program
 *@argv: return the name of argv[i]
 */
-int main(char **argv)
+int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
-	return (0);
+	return (argc);
 }

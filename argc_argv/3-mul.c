4042e7d1464a07f65d6a4d50a16cbc3966cb7fc8
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-07 19:28:31 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,15 +11,17 @@
 
 int main(int argc, char **argv)
 {
-	int y = atoi(argv[2]);
-	int x = atoi(argv[1]);
-	int calc = x * y;
-
+	int x;
+	int y;
+	int calc;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	calc = x * y;
 	printf("%d\n", calc);
 	return (0);
 }

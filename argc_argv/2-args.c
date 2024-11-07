1127b2d75cf6e446c6f616a3f1e85f10f23dc414
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-07 18:27:53 +0100

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,10 +1,18 @@
+/**
+*main - return all the arguments after calling prog
+*@argc: numbers of arguments including prog name
+*@argv: array of arguments including prog name
+*Return: return 0;
+*/
+
 #include "main.h"
 #include <stdio.h>
 
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int i = 0;
+
 	for (i = 0; i < argc; i++)
 	printf("%s\n", argv[i]);
 	return (0);

40492d60f025cbc0ec758aca9a7d15126ff73984
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-12 15:31:40 +0100

    oskour

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,18 +5,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
+	char *p;
 
 	if (size == 0)
-        {
-                return (NULL);
-        }
-	char *p;
+        	return (NULL);
 	p = malloc(sizeof(char) * size);
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 	p[0] = c;
 	for (i = 0; i < size; i++)
 	p[i] = c;

98630b0d0e6c8072fab2debf2e7089082023d7d6
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-12 23:32:34 +0100

    adding script 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*string_nconcat - concatenates two strings but the parameter n define how many
+*from s2.
+*@s1: first string to copy
+*@s2: second string to copy
+*@n: number of bytes we want from s2
+*Return: return the copy of s1 and s2 concatenated,
+*plus terminated by null byte.
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i;
+	unsigned int j;
+	int len;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	len = j += i;
+	len++;
+	p = malloc(sizeof(char) * len);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		p[i] = s1[i];
+	}
+	if (n >= j)
+	{
+		p[i] = *s2;
+		p[i] = '\0';
+		return (p);
+	}
+	for (j = 0; s2[j] != s2[n]; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}

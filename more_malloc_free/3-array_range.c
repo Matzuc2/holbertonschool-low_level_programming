f73a9686ec6d0f306d483e27768a2831f7cbad00
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-13 16:13:46 +0100

    allo

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+*array_range - allocate and create space for an array of min value
+*and max
+*@min: minimum value for array
+*@max: maximum value for array
+*Return: return the array
+*/
 
 int *array_range(int min, int max)
 {
@@ -11,11 +17,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	for (i = 0; i <= max; i++)
-        {
-        }
+	{
+	}
 	p = malloc(sizeof(int) * i);
 	if (p == NULL)
-		return NULL;
+		return (NULL);
 	for (i = min ; i <= max; i++)
 	{
 		p[i] = i;

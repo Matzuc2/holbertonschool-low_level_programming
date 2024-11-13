6ec8afd75383cf36f5cfbd2f95dbd5e26ac6beaf
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-13 13:26:27 +0100

    hey hey

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
-	for (j = 0; j < n s2[j] != '\0'; j++, i++)
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
 		p[i] = s2[j];
 	p[i] = '\0';
 	return (p);

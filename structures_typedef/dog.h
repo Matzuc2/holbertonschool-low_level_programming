305eff0ef9a0d651c779e61a31d9dfe049b4e2b2
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-13 22:48:59 +0100

    adding script dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure for dogs
+ * @name: Pointer to the dog's name (string)
+ * @age: Age of the dog (float)
+ * @owner: Pointer to the owner's name (string)
+ *
+ * Description: This structure represents information about a dog,
+ * including its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+
+#endif

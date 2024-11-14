cc720725c51f999a083ecd0b58722fe934460429
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-14 13:03:06 +0100

    correcting edge case

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
 *init_dog - initialiazes a struct dog
 *@d: pointer to struct d
@@ -8,7 +9,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
 }
+

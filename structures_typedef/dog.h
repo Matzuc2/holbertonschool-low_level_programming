2a95ddc1d7dc223a0c2de0b37ee9d12e16f66f40
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-14 16:01:20 +0100

    adding function 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,14 +10,14 @@
  * including its name, age, and owner's name.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 
 	float age;
 
 	char *owner;
-};
+} dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

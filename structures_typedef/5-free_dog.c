6cc043a57a8e311ec1d2d1caf23d03effde64121
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-15 13:35:41 +0100

    adding script 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*free_dog - dont abandon your dog bro
+*@d: pointeur vers d
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

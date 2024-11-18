73e853ae3db9f8fa61d027d8e5e70ab8ecc08073
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-18 14:01:56 +0100

    adding task function 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int v = 1;
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == v)
+		{
+			return (i);
+		}
+	}
+return (0);
+}

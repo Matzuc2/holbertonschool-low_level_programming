46e96288ddb84257ddb6577bc1f0264171d95295
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-19 13:38:21 +0100

    adding four files for task three of projects functions pointers

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - addition function
+ * @a: int a
+ * @b: int b
+ * Return: return a operating b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function
+ * @a: int a
+ * @b: int b
+ * Return: return a operating b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - * function
+ * @a: int a
+ * @b: int b
+ * Return: return a operating b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - / function
+ * @a: int a
+ * @b: int b
+ * Return: return a operating b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - % addition function
+ * @a: int a
+ * @b: int b
+ * Return: return a operating b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

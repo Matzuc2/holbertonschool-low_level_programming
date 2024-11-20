bbb97e87ad8af27752f15a120223771d1430156d
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-20 10:50:09 +0100

    adding script 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,18 +10,16 @@
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
 va_list args;
+unsigned int i;
 va_start(args, n);
-	if (n == 0)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		if (separator != NULL)
 		{
 			if (i < n - 1)
-			printf("%c ", *separator);
+			printf("%c", *separator);
 		}
 	}
 	printf("\n");

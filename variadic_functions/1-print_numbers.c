f6bd5aa36d8809f0938a2d9f17a533022f6c66ce
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-20 10:37:31 +0100

    adding script 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+*print_numbers - print all numbers after the n using
+*variadics functions
+*@separator: pointer to separator in main, destined
+*to separate each number for clarity
+*@n: print all arguments equal to the number of n;
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL)
+		{
+			if (i < n - 1)
+			printf("%c ", *separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}

f02c85313f954ac63efa550c1bd3f97f19848740
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-21 14:09:01 +0100

    adding last script of project 2

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+*print_int - print integer from args list
+*@args: va_list args
+*/
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+*print_float - print float from args list
+*@args: va_list args
+*/
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+*print_char - print char from args list
+*@args: va_list args
+*/
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+/**
+*print_string - print string from args list
+*@args: va_list args
+*/
+void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", str);
+}
+/**
+*print_all - print va_list after format depending on char
+*on string format
+*@format: string used to know the type of argument in va_list
+*/
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	which_s which[] = {
+	{"i", print_int},
+	{"c", print_char},
+	{"f", print_float},
+	{"s", print_string},
+	{NULL, NULL}
+	};
+
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+	j = 0;
+		while (which[j].w != NULL)
+		{
+			if (*(which[j].w) == format[i])
+			{
+				printf("%s", separator);
+				which[j].f(args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+	i++;
+	}
+	printf("\n");
+	va_end(args);
+}
+
+
+

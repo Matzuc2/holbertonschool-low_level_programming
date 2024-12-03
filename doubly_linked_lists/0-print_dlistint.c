ab58b77fb671bcc7b86d41ad8f4ff084e978e34c
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-03 22:23:42 +0100

    adding script 0 and header file

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Prints all elements of a list_t list
+ * @h: Pointer to the head of the list
+ *
+ * Description: This function traverses a linked list of type list_t,
+ * printing each element's length and string content.
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}

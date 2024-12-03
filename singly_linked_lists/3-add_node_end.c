dee2c64c0ade2f9b20bae3fb0be7ca6f409f2584
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-03 20:46:17 +0100

    adding script 3 and main.h

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to pointer of the head of the list
+ * @str: String to be added as the str element of the new node
+ *
+ * Return: Address of the new element, or NULL if it failed
+ *
+ * Description: This function creates a new node with the given string,
+ * and adds it to the end of the linked list. If the list is empty,
+ * the new node becomes the head of the list.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node_new;
+	list_t *node_end;
+
+	if (str == NULL)
+		return (NULL);
+	node_new = malloc(sizeof(list_t));
+		if (node_new == NULL)
+			return (NULL);
+	node_new->str = strdup(str);
+	node_new->len = strlen(str);
+	node_new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node_new;
+		return (node_new);
+	}
+	node_end = *head;
+	while (node_end->next != NULL)
+	{
+		node_end = node_end->next;
+	}
+	node_end->next = node_new;
+	return (node_new);
+}

055d53b0baac12c3f99614cb15fdd46fe3a3fbc6
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-04 10:18:06 +0100

    adding script 3 to the equation mdr (this project is mi fun mi irritable)

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_dnodeint - Adds a new node at the beginning of a doubly linked list
+ * @head: Double pointer to the head of the list
+ * @n: The value to store in the new node
+ *
+ * Return: Address of the new element, or NULL if it failed
+ *
+ * Description:
+ * This function dynamically allocates memory for a new node in a doubly
+ * linked list, initializes its value to the provided integer `n`, and
+ * inserts it at the beginning of the list. The `prev` pointer of the
+ * new node is set to NULL, and its `next` pointer is set to the current
+ * head of the list. If the list is not empty, the `prev` pointer of the
+ * old head is updated to point to the new node. The head of the list
+ * is then updated to point to the new node.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+
+	return (new_node);
+}

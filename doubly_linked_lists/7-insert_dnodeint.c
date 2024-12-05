9ea68d22d544e957645a7c0cf19599cab654757c
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-05 22:48:42 +0100

    adding script 7 corrected

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,7 +48,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = temp;
 	if (temp->next != NULL)
 		temp->next->prev = new_node;
-		temp->next = new_node;
+
+	temp->next = new_node;
 
 	return (new_node);
 }

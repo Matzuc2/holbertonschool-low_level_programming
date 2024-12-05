40a304f69174a55dba7d46fd9c688d95228899ac
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-05 22:29:50 +0100

    hey

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * sum_dlistint - calculates the sum of all the data (n) in a doubly linked list
+ * sum_dlistint - calculates the sum of all the data (n)
+ * in a doubly linked list
  * @head: pointer to the head of the doubly linked list
  *
  * Return: the sum of all the data (n) in the list,
@@ -11,6 +12,9 @@ int sum_dlistint(dlistint_t *head)
 {
 
 	int sum = 0;
+
+	if (head == NULL)
+		return (0);
 
 	while (head != NULL)
 	{

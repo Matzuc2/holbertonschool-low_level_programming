3bc830540c32636f84c65416b23b316369befdf1
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-05 22:58:36 +0100

    adding script 7 corrected

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
@@ -25,9 +25,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	temp = *h;
 
-	for (i = 0; temp != NULL && i < idx - 1; i++)
+	while (temp != NULL && i < idx - 1)
+	{
 		temp = temp->next;
-
+		i++;
+	}
 
 	if (temp == NULL)
 		return (NULL);
@@ -40,8 +42,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = temp->next;
 	new_node->prev = temp;
+	new_node->next = temp->next;
 	temp->next->prev = new_node;
 	temp->next = new_node;
 

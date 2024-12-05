3e93e6458bde15b25bafc1045d7931bb1a298402
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-05 23:25:45 +0100

    aaaaa

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,13 +23,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	if (*head == NULL)
+
+	if (head == NULL || new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;

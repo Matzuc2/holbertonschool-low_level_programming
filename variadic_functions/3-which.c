a75dfd435665b7e23643e414e0f61028c0cfb5a8
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-05 21:59:54 +0100

    correcting bug from git

---
+++ variadic_functions/3-which.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+int print_int(int integer)
+{
+        /*printf("%d", i);*/
+        return (i);
+}
+float print_float (float f)
+{
+        /*printf("%f", f);*/
+        return (f);
+}
+char print_char(char c)
+{
+        /*printf("%c", c);*/
+        return (c);
+}
+char print_char(char* s)
+{
+        /*printf("%s", s);*/
+        return (s);
+}

019645fc3a04a98d3fcbe1548d6b3e78afedf92b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-12-05 21:47:13 +0100

    allo

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,15 +2,9 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all elements of a list_t list
- * @h: Pointer to the head of the list
- *
- * Description: This function traverses a linked list of type list_t,
- * printing each element's length and string content. If an element's
- * string is NULL, it prints "(nil)" instead. The function also counts
- * the number of nodes in the list.
- *
- * Return: The number of nodes in the list
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
